d3fc91af7876a62710e75aae384dcc6d04f78baa
Elshad98 <elshad.safarov98@gmail.com>
2021-07-29 18:58:48 +0500

    Cache layout. Added features: Save token, Get token

---
+++ app/src/main/java/com/example/chat/data/account/AccountCache.kt
@@ -0,0 +1,12 @@
+package com.example.chat.data.account
+
+import com.example.chat.domain.type.Either
+import com.example.chat.domain.type.None
+import com.example.chat.domain.type.exception.Failure
+
+interface AccountCache {
+
+    fun getToken(): Either<Failure, String>
+
+    fun saveToken(token: String): Either<Failure, None>
+}
---
+++ app/src/main/java/com/example/chat/data/account/AccountRemote.kt
@@ -0,0 +1,16 @@
+package com.example.chat.data.account
+
+import com.example.chat.domain.type.Either
+import com.example.chat.domain.type.None
+import com.example.chat.domain.type.exception.Failure
+
+interface AccountRemote {
+
+    fun register(
+        email: String,
+        name: String,
+        password: String,
+        token: String,
+        userDate: Long
+    ): Either<Failure, None>
+}
