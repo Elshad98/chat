450331ef23b231953a2b33ac5552cf0d9fd19896
Elshad98 <elshad.safarov98@gmail.com>
2021-08-03 18:49:50 +0500

    Domain layer. Added: EmailAlreadyExistError, HAndleOnce

---
+++ app/src/main/java/com/example/chat/domain/type/HandleOnce.kt
@@ -0,0 +1,20 @@
+package com.example.chat.domain.type
+
+open class HandleOnce<out T>(
+    private val content: T
+) {
+
+    private var hasBeenHandled = false
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
--- app/src/main/java/com/example/chat/domain/type/exception/Failure.kt
+++ app/src/main/java/com/example/chat/domain/type/exception/Failure.kt
@@ -7,4 +7,5 @@ sealed class Failure {
 
     object NetworkConnectionError : Failure()
     object ServerError : Failure()
+    object EmailAlreadyExistError: Failure()
 }
