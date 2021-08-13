ce589bcf2ae37c64d6aa38195e411a94b37bc6a2
Elshad98 <elshad.safarov98@gmail.com>
2021-08-13 19:40:37 +0500

    Cache layer. Added: logout, get account, save account

--- app/src/main/java/com/example/chat/cache/AccountCacheImpl.kt
+++ app/src/main/java/com/example/chat/cache/AccountCacheImpl.kt
@@ -1,9 +1,10 @@
 package com.example.chat.cache
 
 import com.example.chat.data.account.AccountCache
+import com.example.chat.domain.account.AccountEntity
 import com.example.chat.domain.type.Either
-import com.example.chat.domain.type.None
 import com.example.chat.domain.type.Failure
+import com.example.chat.domain.type.None
 import javax.inject.Inject
 
 class AccountCacheImpl @Inject constructor(
@@ -16,5 +17,17 @@ class AccountCacheImpl @Inject constructor(
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
--- app/src/main/java/com/example/chat/cache/SharedPrefsManager.kt
+++ app/src/main/java/com/example/chat/cache/SharedPrefsManager.kt
@@ -1,9 +1,10 @@
 package com.example.chat.cache
 
 import android.content.SharedPreferences
+import com.example.chat.domain.account.AccountEntity
 import com.example.chat.domain.type.Either
-import com.example.chat.domain.type.None
 import com.example.chat.domain.type.Failure
+import com.example.chat.domain.type.None
 import javax.inject.Inject
 
 class SharedPrefsManager @Inject constructor(
@@ -12,7 +13,17 @@ class SharedPrefsManager @Inject constructor(
 
     companion object {
 
-        const val ACCOUNT_TOKEN = "account_token"
+        private const val ACCOUNT_TOKEN = "account_token"
+        private const val ACCOUNT_ID = "account_id"
+        private const val ACCOUNT_NAME = "account_name"
+        private const val ACCOUNT_EMAIL = "account_email"
+        private const val ACCOUNT_STATUS = "account_status"
+        private const val ACCOUNT_DATA = "account_data"
+        private const val ACCOUNT_IMAGE = "account_image"
+    }
+
+    fun getToken(): Either<Failure, String> {
+        return Either.Right(prefs.getString(ACCOUNT_TOKEN, "")!!)
     }
 
     fun saveToken(token: String): Either<Failure, None> {
@@ -23,7 +34,50 @@ class SharedPrefsManager @Inject constructor(
         return Either.Right(None())
     }
 
-    fun getToken(): Either<Failure, String> {
-        return Either.Right(prefs.getString(ACCOUNT_TOKEN, "")!!)
+    fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        prefs.edit().apply {
+            putLong(ACCOUNT_ID, account.id)
+            putString(ACCOUNT_NAME, account.name)
+            putString(ACCOUNT_EMAIL, account.email)
+            putString(ACCOUNT_TOKEN, account.token)
+            putString(ACCOUNT_STATUS, account.status)
+            putLong(ACCOUNT_DATA, account.userData)
+            putString(ACCOUNT_IMAGE, account.image)
+        }.apply()
+
+        return Either.Right(None())
+    }
+
+    fun getAccount(): Either<Failure, AccountEntity> {
+        val id = prefs.getLong(ACCOUNT_ID, 0)
+
+        if (id == 0L) {
+            return Either.Left(Failure.NoSavedAccountsError)
+        }
+
+        val account = AccountEntity(
+            prefs.getLong(ACCOUNT_ID, 0),
+            prefs.getString(ACCOUNT_NAME, "")!!,
+            prefs.getString(ACCOUNT_EMAIL, "")!!,
+            prefs.getString(ACCOUNT_TOKEN, "")!!,
+            prefs.getString(ACCOUNT_STATUS, "")!!,
+            prefs.getLong(ACCOUNT_DATA, 0),
+            prefs.getString(ACCOUNT_IMAGE, "")!!
+        )
+
+        return Either.Right(account)
+    }
+
+    fun removeAccount(): Either<Failure, None> {
+        prefs.edit().apply {
+            remove(ACCOUNT_ID)
+            remove(ACCOUNT_NAME)
+            remove(ACCOUNT_EMAIL)
+            remove(ACCOUNT_STATUS)
+            remove(ACCOUNT_DATA)
+            remove(ACCOUNT_IMAGE)
+        }.apply()
+
+        return Either.Right(None())
     }
 }
