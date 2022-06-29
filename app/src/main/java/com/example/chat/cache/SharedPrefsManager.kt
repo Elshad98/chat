02ea41fb5fea79510019c45ad6d977a95dd13c8c
Elshad98 <elshad.safarov98@gmail.com>
2022-06-29 20:02:45 +0500

    Cache layer. Added: safely putting parameters

--- app/src/main/java/com/example/chat/cache/SharedPrefsManager.kt
+++ app/src/main/java/com/example/chat/cache/SharedPrefsManager.kt
@@ -13,22 +13,23 @@ class SharedPrefsManager @Inject constructor(
 
     companion object {
 
-        private const val ACCOUNT_ID = "account_id"
-        private const val ACCOUNT_NAME = "account_name"
-        private const val ACCOUNT_DATA = "account_data"
-        private const val ACCOUNT_EMAIL = "account_email"
-        private const val ACCOUNT_IMAGE = "account_image"
-        private const val ACCOUNT_TOKEN = "account_token"
-        private const val ACCOUNT_STATUS = "account_status"
+        private const val PREF_ACCOUNT_ID = "account_id"
+        private const val PREF_ACCOUNT_NAME = "account_name"
+        private const val PREF_ACCOUNT_DATA = "account_data"
+        private const val PREF_ACCOUNT_EMAIL = "account_email"
+        private const val PREF_ACCOUNT_IMAGE = "account_image"
+        private const val PREF_ACCOUNT_TOKEN = "account_token"
+        private const val PREF_ACCOUNT_STATUS = "account_status"
+        private const val PREF_ACCOUNT_PASSWORD = "account_password"
     }
 
     fun getToken(): Either<Failure, String> {
-        return Either.Right(prefs.getString(ACCOUNT_TOKEN, "")!!)
+        return Either.Right(prefs.getString(PREF_ACCOUNT_TOKEN, "")!!)
     }
 
     fun saveToken(token: String): Either<Failure, None> {
         prefs.edit().apply {
-            putString(ACCOUNT_TOKEN, token)
+            putString(PREF_ACCOUNT_TOKEN, token)
         }.apply()
 
         return Either.Right(None())
@@ -36,53 +37,66 @@ class SharedPrefsManager @Inject constructor(
 
     fun saveAccount(account: AccountEntity): Either<Failure, None> {
         prefs.edit().apply {
-            putLong(ACCOUNT_ID, account.id)
-            putString(ACCOUNT_NAME, account.name)
-            putLong(ACCOUNT_DATA, account.userData)
-            putString(ACCOUNT_EMAIL, account.email)
-            putString(ACCOUNT_TOKEN, account.token)
-            putString(ACCOUNT_IMAGE, account.image)
-            putString(ACCOUNT_STATUS, account.status)
+            putSafely(PREF_ACCOUNT_ID, account.id)
+            putSafely(PREF_ACCOUNT_NAME, account.name)
+            putSafely(PREF_ACCOUNT_EMAIL, account.email)
+            putSafely(PREF_ACCOUNT_TOKEN, account.token)
+            putSafely(PREF_ACCOUNT_IMAGE, account.image)
+            putString(PREF_ACCOUNT_STATUS, account.status)
+            putSafely(PREF_ACCOUNT_DATA, account.userData)
+            putSafely(PREF_ACCOUNT_PASSWORD, account.password)
         }.apply()
 
         return Either.Right(None())
     }
 
     fun getAccount(): Either<Failure, AccountEntity> {
-        val id = prefs.getLong(ACCOUNT_ID, 0)
+        val id = prefs.getLong(PREF_ACCOUNT_ID, 0)
 
         if (id == 0L) {
             return Either.Left(Failure.NoSavedAccountsError)
         }
 
         val account = AccountEntity(
-            id = prefs.getLong(ACCOUNT_ID, 0),
-            userData = prefs.getLong(ACCOUNT_DATA, 0),
-            name = prefs.getString(ACCOUNT_NAME, "")!!,
-            email = prefs.getString(ACCOUNT_EMAIL, "")!!,
-            image = prefs.getString(ACCOUNT_IMAGE, "")!!,
-            token = prefs.getString(ACCOUNT_TOKEN, "")!!,
-            status = prefs.getString(ACCOUNT_STATUS, "")!!
+            id = id,
+            userData = prefs.getLong(PREF_ACCOUNT_DATA, 0),
+            name = prefs.getString(PREF_ACCOUNT_NAME, "")!!,
+            email = prefs.getString(PREF_ACCOUNT_EMAIL, "")!!,
+            image = prefs.getString(PREF_ACCOUNT_IMAGE, "")!!,
+            token = prefs.getString(PREF_ACCOUNT_TOKEN, "")!!,
+            status = prefs.getString(PREF_ACCOUNT_STATUS, "")!!,
+            password = prefs.getString(PREF_ACCOUNT_PASSWORD, "")!!
         )
-
         return Either.Right(account)
     }
 
     fun removeAccount(): Either<Failure, None> {
         prefs.edit().apply {
-            remove(ACCOUNT_ID)
-            remove(ACCOUNT_NAME)
-            remove(ACCOUNT_DATA)
-            remove(ACCOUNT_EMAIL)
-            remove(ACCOUNT_IMAGE)
-            remove(ACCOUNT_STATUS)
+            remove(PREF_ACCOUNT_ID)
+            remove(PREF_ACCOUNT_NAME)
+            remove(PREF_ACCOUNT_DATA)
+            remove(PREF_ACCOUNT_EMAIL)
+            remove(PREF_ACCOUNT_IMAGE)
+            remove(PREF_ACCOUNT_STATUS)
+            remove(PREF_ACCOUNT_PASSWORD)
         }.apply()
 
         return Either.Right(None())
     }
 
     fun containsAnyAccount(): Boolean {
-        val id = prefs.getLong(ACCOUNT_ID, 0)
-        return id != 0L
+        return prefs.getLong(PREF_ACCOUNT_ID, 0) != 0L
+    }
+}
+
+private fun SharedPreferences.Editor.putSafely(key: String, value: Long?) {
+    if (value != null && value != 0L) {
+        putLong(key, value)
+    }
+}
+
+private fun SharedPreferences.Editor.putSafely(key: String, value: String?) {
+    if (value?.isNotEmpty() == true) {
+        putString(key, value)
     }
 }
