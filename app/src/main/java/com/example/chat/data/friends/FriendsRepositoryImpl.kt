5d16e306beace58252c89f4abcb34e72f5750119
Elshad98 <elshad.safarov98@gmail.com>
2022-07-18 13:16:45 +0500

    Data layer. Added: friends caching

--- app/src/main/java/com/example/chat/data/friends/FriendsRepositoryImpl.kt
+++ app/src/main/java/com/example/chat/data/friends/FriendsRepositoryImpl.kt
@@ -7,38 +7,59 @@ import com.example.chat.domain.type.Either
 import com.example.chat.domain.type.Failure
 import com.example.chat.domain.type.None
 import com.example.chat.domain.type.flatMap
+import com.example.chat.domain.type.onNext
 
 class FriendsRepositoryImpl(
+    private val friendsCache: FriendsCache,
     private val accountCache: AccountCache,
     private val friendsRemote: FriendsRemote
 ) : FriendsRepository {
 
-    override fun getFriends(): Either<Failure, List<FriendEntity>> {
+    override fun getFriends(needFetch: Boolean): Either<Failure, List<FriendEntity>> {
         return accountCache
             .getCurrentAccount()
-            .flatMap { accountEntity ->
-                friendsRemote.getFriends(accountEntity.id, accountEntity.token)
+            .flatMap { account ->
+                if (needFetch) {
+                    friendsRemote.getFriends(account.id, account.token)
+                } else {
+                    Either.Right(friendsCache.getFriends())
+                }
             }
+            .onNext { friends -> friends.map(friendsCache::saveFriend) }
     }
 
-    override fun getFriendRequest(): Either<Failure, List<FriendEntity>> {
+    override fun getFriendRequest(needFetch: Boolean): Either<Failure, List<FriendEntity>> {
         return accountCache
             .getCurrentAccount()
-            .flatMap { accountEntity ->
-                friendsRemote.getFriendRequests(accountEntity.id, accountEntity.token)
+            .flatMap { account ->
+                if (needFetch) {
+                    friendsRemote.getFriends(account.id, account.token)
+                } else {
+                    Either.Right(friendsCache.getFriends())
+                }
+            }
+            .onNext { friends ->
+                friends.map { friend ->
+                    friend.isRequest = 1
+                    friendsCache.saveFriend(friend)
+                }
             }
     }
 
     override fun approveFriendRequest(friendEntity: FriendEntity): Either<Failure, None> {
         return accountCache
             .getCurrentAccount()
-            .flatMap { accountEntity ->
+            .flatMap { account ->
                 friendsRemote.approveFriendRequest(
-                    accountEntity.id,
+                    account.id,
                     friendEntity.id,
                     friendEntity.friendsId,
-                    accountEntity.token
+                    account.token
                 )
+            }
+            .onNext {
+                friendEntity.isRequest = 0
+                friendsCache.saveFriend(friendEntity)
             }
     }
 
@@ -53,13 +74,14 @@ class FriendsRepositoryImpl(
                     accountEntity.token
                 )
             }
+            .onNext { friendsCache.deleteFriend(friendEntity.id) }
     }
 
     override fun addFriend(email: String): Either<Failure, None> {
         return accountCache
             .getCurrentAccount()
-            .flatMap { accountEntity ->
-                friendsRemote.addFriend(email, accountEntity.id, accountEntity.token)
+            .flatMap { account ->
+                friendsRemote.addFriend(email, account.id, account.token)
             }
     }
 
@@ -74,5 +96,6 @@ class FriendsRepositoryImpl(
                     accountEntity.token
                 )
             }
+            .onNext { friendsCache.deleteFriend(friendEntity.id) }
     }
 }
